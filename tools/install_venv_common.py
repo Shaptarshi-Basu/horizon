3a43ba9f1b953a0868fe0ac2aed59b47f6ab660a
Shaptarshi-Basu <shapbasu@gmail.com>
2018-02-20 23:20:46 +0200

    Update install_venv_common.py

--- tools/install_venv_common.py
+++ tools/install_venv_common.py
@@ -116,7 +116,7 @@ class InstallVenv(object):
         self.pip_install('pip>=1.4')
         self.pip_install('setuptools')
         self.pip_install('pbr')
-
+        self.pip_install('six')
         self.pip_install('-r', self.requirements, '-r', self.test_requirements)
 
     def parse_args(self, argv):
